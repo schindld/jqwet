8ec92fee569e50145e43a265afe762bb11795ba4
schindld <dave.schindler@gmail.com>
2012-04-22 14:55:10 -0400

    Deleted pe-ap2.js which used Dustin Diaz's $script.js, because all the callbacks from loaded scripts ran serially, blocking the UI too much.

---
+++ pe-inspector.js
@@ -0,0 +1,54 @@
+﻿(function inspect(parent, depth) {
+	var console = this.console ? this.console : {
+			log: function() {}
+		},
+		indent = "",
+		prop,
+		result;
+	if (depth === 0) {
+		console.log("pe");
+	}
+	for (var d = 0; d < depth; d++) {
+		indent += "\u250a  ";
+	}
+	for (property in parent) {
+		if (parent.hasOwnProperty(property)) {
+			prop = parent[property];
+			switch (typeof prop) {
+			case 'object':
+				if (Object.prototype.toString.apply(prop) === '[object Array]') {
+					console.log(indent + " \u2937." + property + " :: Array");
+				} else if (typeof prop.nodeName === 'string') {
+					console.log(indent + " \u2937." + property + " :: DOM object");
+				} else if (depth < 3) {
+					console.log(indent + " \u2937." + property + " :: object");
+					inspect(prop, depth + 1);
+				}
+				break;
+			case 'function':
+				if (prop.length === 0) {
+					result = prop();
+					switch (typeof result) {
+					case 'undefined':
+						console.log(indent + " \u2937." + property + " :: function, returns void");
+						break;
+					default:
+						if (typeof result.jquery === 'string') {
+							console.log(indent + " \u2937." + property + " :: function, returns jQuery object");
+						} else if (typeof result.nodeName === 'string') {
+							console.log(indent + " \u2937." + property + " :: function, returns DOM object");
+						} else {
+							console.log(indent + " \u2937." + property + " :: function, returns " + typeof result);
+						}
+					}
+				} else {
+					console.log(indent + " \u2937." + property + " :: function, returns ?");
+				}
+				break;
+			default:
+				console.log(indent + " \u2937." + property + " :: " + typeof prop);
+			}
+		}
+	}
+	return true;
+}(pe, 0));
