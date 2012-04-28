120e79ebc3f336472017d183fe9d7fa093adffb4
schindld <dave.schindler@gmail.com>
2012-04-28 12:23:26 -0400

    Cleaned redundant variables.

--- js/workers/pluginTemplate.js
+++ js/workers/pluginTemplate.js
@@ -11,33 +11,30 @@
         depends: ['easytabs', 'equalheights'],
 		// Don't include a mobile function if your plugin shouldn't run in mobile mode.
 		mobile: function (elm) {
-			var $this;
-			$this = elm;
 			// If applicaple, convert html elements and attributes into the format that jQuery mobile expects.
 			
-			return $this;
+			return elm;
 		},
         _exec: function (elm) {
 			// Don't include this if statement if your plugin shouldn't run in mobile mode.
 			if (pe.mobile) {
 				return _pe.fn.pluginName.mobile(elm);
 			}
-			var $this, opts, aVariable, anotherVariable;
-			$this = elm;
+			var opts, aVariable, anotherVariable;
 			opts = {
 				// This is an example from tabbedinterface, to show how to pass configuration parameters from the html element to the plugin.
 				// There are some simple examples here, along with some more complicated ones.
-				defaultElm: (($this.find(".default").length) ? ".default" : "li:first-child"),
-				autoHeight: ($this.hasClass("auto-height-none") ? false : true),
-				cycle: ($this.hasClass("cycle-slow") ? 8000 : ($this.hasClass("cycle-fast") ? 2000 : ($this.hasClass("cycle") ? 6000 : false))),
-				carousel: (/style-carousel/i.test($this.attr('class'))) ? true : false,
-				autoPlay: ($this.hasClass("auto-play") ? true : false),
-				animate: ($this.hasClass("animate") || $this.hasClass("animate-slow") || $this.hasClass("animate-fast") ? true : false),
-				animationSpeed: ($this.hasClass("animate-slow") ? "slow" : ($this.hasClass("animate-fast") ? "fast" : "normal"))
+				defaultElm: ((elm.find(".default").length) ? ".default" : "li:first-child"),
+				autoHeight: (elm.hasClass("auto-height-none") ? false : true),
+				cycle: (elm.hasClass("cycle-slow") ? 8000 : (elm.hasClass("cycle-fast") ? 2000 : (elm.hasClass("cycle") ? 6000 : false))),
+				carousel: (/style-carousel/i.test(elm.attr('class'))) ? true : false,
+				autoPlay: (elm.hasClass("auto-play") ? true : false),
+				animate: (elm.hasClass("animate") || elm.hasClass("animate-slow") || elm.hasClass("animate-fast") ? true : false),
+				animationSpeed: (elm.hasClass("animate-slow") ? "slow" : (elm.hasClass("animate-fast") ? "fast" : "normal"))
 			};
 			// Do plugin stuff here...
 			
-			return $this;
+			return elm;
         } // end of exec
     };
     window.pe = _pe;
