260b68a569c134d8ed3db624b2dc5bf1345b62d3
schindld <dave.schindler@gmail.com>
2012-05-01 17:19:45 -0400

    Moved documentation here (gh-pages) from master branch. Can be viewed online at http://schindld.github.com/jqwet/.

---
+++ js/workers/zebra.js
@@ -0,0 +1,44 @@
+ /*!
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * www.tbs.gc.ca/ws-nw/wet-boew/terms / www.sct.gc.ca/ws-nw/wet-boew/conditions
+ */
+/*
+ * Zebra stripping functionality for block level elements
+ */
+(function ($) {
+	var _pe = window.pe || {
+		fn : {}
+	};
+	/* local reference */
+	_pe.fn.zebra = {
+		type : 'plugin',
+		_exec : function (elm) {
+			var $trs,
+				$lis,
+				parity;
+			if (elm.is('table')) {
+				$trs = (elm.children('tr').add(elm.children('tbody').children('tr'))).filter(function () {
+					return $(this).children('th').length === 0;
+				});
+				// note: even/odd's indices start at 0
+				$trs.filter(':odd').addClass('table-even');
+				$trs.filter(':even').addClass('table-odd');
+				$trs.on('td', 'hover focusin focusout', function (e) {
+					e.stopPropagation();
+					$(this).closest('tr').toggleClass('table-hover');
+				});
+			} else {
+				$lis = elm.children('li');
+				parity = (elm.parents('li').length + 1) % 2;
+				$lis.filter(':odd').addClass(parity === 0 ? 'list-odd' : 'list-even');
+				$lis.filter(':even').addClass(parity === 1 ? 'list-odd' : 'list-even');
+				$lis.on('mouseover mouseout focusin focusout', function (e) {
+					e.stopPropagation();
+					$(this).toggleClass('list-hover');
+				});
+			}
+		} // end of exec
+	};
+	window.pe = _pe;
+	return _pe;
+}(jQuery));
